b1a13c585efb4ca8a0c71a184e88f754a012309d
yorgis711 <105851696+yorgis711@users.noreply.github.com>
2022-09-24 10:44:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -56,16 +56,7 @@
        
 	
 	
-            <!-- end: PAGE FOOTER-->
-            
-					 <?php
-session_start();
-if (!isset($_SESSION['count'])) {
-  $_SESSION['count'] = 0;
-} else {
-  $_SESSION['count']++;
-}
-?>
+
 				
 				
 				
